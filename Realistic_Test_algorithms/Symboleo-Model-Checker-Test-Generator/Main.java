9230ccdca1e75a84937f1f9dcdc0975cd8841384
andaamal <65017529+andaamal@users.noreply.github.com>
2024-01-23 20:56:48 -0500

    Add files via upload

---
+++ Realistic_Test_algorithms/Symboleo-Model-Checker-Test-Generator/Main.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.File;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		if(args.length < 4)
+			System.out.println("Missing arguments: java Main <obligations number> <powers number> <properties number>");
+		Test test = new Test(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+		test.set_nuxmv_file("nuXmv");
+		// make directory
+		File output = new File("result");
+		if(!output.exists()){
+			output.mkdirs();
+		}
+		test.set_output_folder("result");
+		test.generate();
+		test.run();
+	}
+
+}
